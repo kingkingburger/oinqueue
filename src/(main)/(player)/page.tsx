e1ff1639c81081dc65051903b5a43a7c52e2e72e
minho <dnjsalsgh123@gmail.com>
2025-06-05 10:51:58 +0900

    update: show last 3 match info

--- src/(main)/(player)/page.tsx
+++ src/(main)/(player)/page.tsx
@@ -13,14 +13,5 @@ interface PlayerProps {
 }
 
 export const Player: React.FC<PlayerProps> = async ({ gameName, tagName }) => {
-	const summonerInfo = await getRiotSummonerInfo(gameName, tagName);
-	const matchIds = await getMatchList(summonerInfo.puuid);
-	const matchInfo = await getMatchInfo(matchIds[3]);
-
-	for (const participant of matchInfo.metadata.participants) {
-		const summonerInfo = await getRiotSummonerInfoByPuuid(participant);
-		// console.log("summonerInfo = ", summonerInfo.gameName);
-	}
-
 	return <div>player layout</div>;
 };
