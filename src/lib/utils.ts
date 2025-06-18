4093ca0af741691ff4ae0a586bbac9efa7cca11e
minho <dnjsalsgh123@gmail.com>
2025-06-18 10:04:54 +0900

    update: restore to original state lib/utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,40 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import ky, { type HTTPError } from "ky";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+// 1) 환경변수 이름을 실제 역할에 맞게 교정
+// riotApiUrl: Riot API의 기본 URL
+// riotApiKey: Riot API 키
+export const riotApiUrl = process.env.NEXT_PUBLIC_RIOT_API_URL;
+export const riotApiKey = process.env.NEXT_PUBLIC_RIOT_API_KEY;
+
+export const useRiotApiEngine = () =>
+	ky.create({
+		prefixUrl: riotApiUrl,
+		timeout: 10000,
+		retry: 1,
+		searchParams: {
+			api_key: riotApiKey || "",
+		},
+		hooks: {
+			beforeError: [
+				async (error: HTTPError<unknown>) => {
+					const { response } = error;
+					if (response?.body) {
+						try {
+							const errorData: Record<string, string> = await response.json();
+							error.message =
+								errorData.detail || errorData.message || error.message;
+						} catch {
+							error.message = await response.text();
+						}
+					}
+					return error;
+				},
+			],
+		},
+	});
